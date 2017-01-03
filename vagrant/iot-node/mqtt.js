1e0737b69ba100d46c26a0d51887977fb10d411e
Giacomo Napoli <giacomonapoli@gmail.com>
2017-01-03 19:05:32 +0100

    testing angular js

--- vagrant/iot-node/mqtt.js
+++ vagrant/iot-node/mqtt.js
@@ -1,12 +1,12 @@
-var mqtt = require('mqtt')
+var mqtt = require('mqtt');
 
-var client = mqtt.connect('mqtt://iot.eclipse.org')
+var client = mqtt.connect('mqtt://iot.eclipse.org');
 
 
 client.on('connect', () => {
   client.subscribe('iotenv/1');
-})
+});
 
 client.on('message', (topic, message) => {
     console.log(message.toString());
-})
+});
